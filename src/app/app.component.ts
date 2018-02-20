52ab7589cae31071c0f6894305fe8c61b2ee92cd
newcomj <jeremy.newcomb@igt.com>
2018-02-20 11:30:36 -0800

    Added product-list.component.html and added html to the file to display a product table. No data is binded to this view currently

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div><h1>{{pageTitle}}</h1>
-      <div>My First Component</div>
-    </div>
-  `
+  template: 'product-list.component.html'
 })
 
 export class AppComponent {
